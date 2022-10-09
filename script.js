98bb741f196e9b99612ae6bb8d280671e675220e
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-10-09 00:27:19 -0700

    added forecast by filtering out 5 days to display

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ $(function saveCity() {
     $(btn).on("click", function (event) {
         event.preventDefault(); // so won't refresh
         var cityName = cityBox.val().trim();
-        var fetchWeather = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=62a0c9b6ffbd35cd6b887afaa5e015c1&units=imperial&"
-        var fetchCW = ""
+        var fetchWeather = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=62a0c9b6ffbd35cd6b887afaa5e015c1&units=imperial"
+        var fetchFuture = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=62a0c9b6ffbd35cd6b887afaa5e015c1&units=imperial"
         console.log(cityName);
         // localStorage.setItem(id, toSave);
         $.when($.ajax({
@@ -57,16 +57,14 @@ $(function saveCity() {
             //deciding what icon to show based on weather
             var iconURL = $("<img src='http://openweathermap.org/img/wn/" + icon + "@2x.png' class='col-4'></img>");
             nowCon.prepend(iconURL);
-
-
             //adding button for recall
             var recallBtn = $("<btn class='btn btn-info mw-100 col-12'>" + city.name + "</btn>");
             recallBtn.attr("data-citi", name);
             recallBtn.text(name);
             $(savBox).append(recallBtn);
-            console.log('test')
-            console.log(date.toLocaleString());
-            console.log(feh);
+            // console.log('test')
+            // console.log(date.toLocaleString());
+            // console.log(feh);
             // console.log(city);
             // localStorage.setItem(name, [name, lon, lat]);
             // console.log(JSON.stringify(lon));
@@ -74,13 +72,62 @@ $(function saveCity() {
             // $.ajax({
             //     url:
             // })
-            // }).then(function (city) {//might as well make the button for recall here
-            //then get the next
-        }));
+            // fetching 5 day forecast.
+        }).then($.ajax({
+            url: fetchFuture,
+            method: 'GET',
+        }).then(function (forecast) {
+            // console.log(forecast);
+            // displayD.text("sysTime: " + date.toLocaleString());
+            for (var i = 0; i < forecast.list.length; i++) {
+                var aux = 1;
+                var dt = eval(forecast.list[i].dt * 1000);
+                console.log(dt);
+                var T = new Date(dt);
+                console.log(T);
+                var date = ("<div>" + T.getMonth() + "/" + T.getDate() + "</div>");
+                console.log(date); // to display 5-day forecast dates
+                var feh = (forecast.list[i].main.temp);
+                var wind = (forecast.list[i].wind.speed);
+                var hum = (forecast.list[i].main.humidity);
+                var icon = (forecast.list[i].weather[0].icon);
+                var cond = (forecast.list[i].weather[0].main);
+                var iconURL = $("<img src='http://openweathermap.org/img/wn/" + icon + "@2x.png'></img>");
+                var card = $("<li class ='card'></li>");
+                card.attr("data-day", aux);
+                card.
+                    card.append(iconURL);
+                card.append(date);
+                card.append(feh);
+                card.append(wind);
+                card.append(hum);
+                futCon.append(card);
+                aux++;
+                i = i + 7;
+
+                // card.append(date, feh, wind, hum);
+                // cTemp.text("Temp: " + feh + " F");
+                // nowCon.append(fTemp);
+                // var fWind = $("<li class='list-group-item' id= 'cWind'></li>");
+                // cWind.text("Wind: " + wind + " MPH");
+                // nowCon.append(fWind);
+                // var fHum = $("<li class='list-group-item' id= 'cHum'></li>");
+                // cHum.text("Humidity: " + hum + " %");
+                // nowCon.append(cHum);
+                // //deciding what icon to show based on weather
+                // nowCon.prepend(iconURL);
 
 
 
-    })
+            }
+
+        })
+
+        ))
+    });
+
+
+
 });
 
 //lets fetch some data
