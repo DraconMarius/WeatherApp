cfb901c029cf571dfae6d9868a670cef02394140
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-10-09 19:54:58 -0700

    trying to push a fix for weather api at deployed link

--- script.js
+++ script.js
@@ -21,15 +21,15 @@ $(function saveCity() {
     $(btn).on("click", function fetch(event) {
         event.preventDefault(); // so won't refresh
         var cityName = cityBox.val().trim();
-        var fetchWeather = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=62a0c9b6ffbd35cd6b887afaa5e015c1&units=imperial"
-        var fetchFuture = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=62a0c9b6ffbd35cd6b887afaa5e015c1&units=imperial"
-        console.log(cityName);
+        var fetchWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=62a0c9b6ffbd35cd6b887afaa5e015c1&units=imperial"
+        var fetchFuture = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=62a0c9b6ffbd35cd6b887afaa5e015c1&units=imperial"
+        // console.log(cityName);
         // localStorage.setItem(id, toSave);
         $.when($.ajax({
             url: fetchWeather,
             method: 'GET',
         }).then(function (city) {
-            console.log(city);
+            // console.log(city);
             var dt = eval(city.dt * 1000);
             var date = new Date(dt);
             var feh = (city.main.temp);
@@ -46,7 +46,7 @@ $(function saveCity() {
             displayD.text("sysTime: " + date.toLocaleString());
             nameBox.append(displayD);
             var cTemp = $("<li class='list-group-item' id= 'cTemp'></li>");
-            cTemp.text("Temp: " + feh + " F");
+            cTemp.text("Temp: " + feh + " °F");
             nowCon.append(cTemp);
             var cWind = $("<li class='list-group-item' id= 'cWind'></li>");
             cWind.text("Wind: " + wind + " MPH");
@@ -55,7 +55,7 @@ $(function saveCity() {
             cHum.text("Humidity: " + hum + " %");
             nowCon.append(cHum);
             //deciding what icon to show based on weather
-            var iconURL = $("<img src='http://openweathermap.org/img/wn/" + icon + "@2x.png' class='min-vw-15 mx-auto d-block' title='Current Weather: " + weather + "'></img>");
+            var iconURL = $("<img src='https://openweathermap.org/img/wn/" + icon + "@2x.png' class='min-vw-15 mx-auto d-block' title='Current Weather: " + weather + "'></img>");
             nowCon.prepend(iconURL);
             //adding button for recall
             var recallBtn = $("<btn class='btn btn-outline-warning mw-100 col-12 mb-3 recallBtn'>" + city.name + "</btn>");
@@ -72,19 +72,22 @@ $(function saveCity() {
             futCon.empty();
             var aux = 1;
             // displayD.text("sysTime: " + date.toLocaleString());
-            for (var i = 0; i < forecast.list.length; i++) {
+            // oh no, I should have just print the dt_text
+            ////starts at 6am utc (thats why i = 1)
+            for (var i = 1; i < forecast.list.length; i++) {
                 var dt = eval(forecast.list[i].dt * 1000);
-                console.log(dt);
+                // console.log(forecast.list[i]);
                 var T = new Date(dt);
-                console.log(T); //we then use getMonth + 1 cuz apparently it counts from 0 index)
-                var date = ("<div class='text-center fiveDate'>" + (T.getMonth() + 1) + "/" + T.getDate() + "</div>");
-                console.log(date); // to display 5-day forecast dates
-                var feh = $("<li class='list-group-item'>Temp: " + forecast.list[i].main.temp + " F</li>");
+                // console.log(forecast.list[i].dt_txt); //we then use + 1 t0 both Month and date cuz apparently it counts from 0 index orz
+                var date = ("<div class='text-center fiveDate'>" + (T.getMonth() + 1) + "/" + (T.getDate() + 1) + "</div>");
+                // var date = ("<div class='text-center fiveDate'>" + forecast.list[i].dt_txt + "</div>");
+                // console.log(date); // to display 5-day forecast dates
+                var feh = $("<li class='list-group-item'>Temp: " + forecast.list[i].main.temp + " °F</li>");
                 var wind = $("<li class='list-group-item'>Wind: " + forecast.list[i].wind.speed + " MPH</li>");
                 var hum = $("<li class='list-group-item'>Hum :" + forecast.list[i].main.humidity + " %</li>");
                 var icon = (forecast.list[i].weather[0].icon);
                 var cond = (forecast.list[i].weather[0].main);
-                var iconURL = $("<img src='http://openweathermap.org/img/wn/" + icon + "@2x.png' class = 'min-vw-15 mx-auto d-block' title='Day " + aux + " Weather: " + cond + "'></img>");
+                var iconURL = $("<img src='https://openweathermap.org/img/wn/" + icon + "@2x.png' class = 'min-vw-15 mx-auto d-block' title='Day " + aux + " Weather: " + cond + "'></img>");
                 var card = $("<p class ='card mh-100 h-auto d-inline-block align-items-center justify-content-center'></p>");
                 // card.attr("data-day", aux);
                 card.append(iconURL);
@@ -95,7 +98,7 @@ $(function saveCity() {
                 card.prepend(iconURL);
                 futCon.append(card);
                 aux++;
-                i = i + 7;
+                i = i + 7; //fast forward 24 hrs including the original increments on line 158
             }
         })
         ))
@@ -106,8 +109,8 @@ $(function (event) {
     $(savBox).on("click", ".recallBtn", function () {
         var cityName = $(this).attr("data-citi");
         // console.log(cityName);
-        var fetchWeather = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=62a0c9b6ffbd35cd6b887afaa5e015c1&units=imperial"
-        var fetchFuture = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=62a0c9b6ffbd35cd6b887afaa5e015c1&units=imperial"
+        var fetchWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=62a0c9b6ffbd35cd6b887afaa5e015c1&units=imperial"
+        var fetchFuture = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=62a0c9b6ffbd35cd6b887afaa5e015c1&units=imperial"
         // console.log(cityName);
         // localStorage.setItem(id, toSave);
         $.when($.ajax({
@@ -130,7 +133,7 @@ $(function (event) {
             displayD.text("sysTime: " + date.toLocaleString());
             nameBox.append(displayD);
             var cTemp = $("<li class='list-group-item' id= 'cTemp'></li>");
-            cTemp.text("Temp: " + feh + " F");
+            cTemp.text("Temp: " + feh + " °F");
             nowCon.append(cTemp);
             var cWind = $("<li class='list-group-item' id= 'cWind'></li>");
             cWind.text("Wind: " + wind + " MPH");
@@ -139,7 +142,7 @@ $(function (event) {
             cHum.text("Humidity: " + hum + " %");
             nowCon.append(cHum);
             //deciding what icon to show based on weather
-            var iconURL = $("<img src='http://openweathermap.org/img/wn/" + icon + "@2x.png' class='min-vw-15 mx-auto d-block' title='Current Weather: " + weather + "'></img>");
+            var iconURL = $("<img src='https://openweathermap.org/img/wn/" + icon + "@2x.png' class='min-vw-15 mx-auto d-block' title='Current Weather: " + weather + "'></img>");
             nowCon.prepend(iconURL);
             // No Button this time
             // fetching 5 day forecast.
@@ -152,19 +155,20 @@ $(function (event) {
             futCon.empty();
             var aux = 1;
             // displayD.text("sysTime: " + date.toLocaleString());
-            for (var i = 0; i < forecast.list.length; i++) {
+            //starts at 6am utc (thats why i = 1)
+            for (var i = 1; i < forecast.list.length; i++) {
                 var dt = eval(forecast.list[i].dt * 1000);
-                console.log(dt);
+                // console.log(dt);
                 var T = new Date(dt);
-                console.log(T); //we then use getMonth + 1 cuz apparently it counts from 0 index)
-                var date = ("<div class='text-center fiveDate'>" + (T.getMonth() + 1) + "/" + T.getDate() + "</div>");
-                console.log(date); // to display 5-day forecast dates
-                var feh = $("<li class='list-group-item'>Temp: " + forecast.list[i].main.temp + " F</li>");
+                // console.log(T); //we then use getMonth + 1 cuz apparently it counts from 0 index)
+                var date = ("<div class='text-center fiveDate'>" + (T.getMonth() + 1) + "/" + (T.getDate() + 1) + "</div>");
+                // console.log(date); // to display 5-day forecast dates
+                var feh = $("<li class='list-group-item'>Temp: " + forecast.list[i].main.temp + " °F</li>");
                 var wind = $("<li class='list-group-item'>Wind: " + forecast.list[i].wind.speed + " MPH</li>");
                 var hum = $("<li class='list-group-item'>Hum :" + forecast.list[i].main.humidity + " %</li>");
                 var icon = (forecast.list[i].weather[0].icon);
                 var cond = (forecast.list[i].weather[0].main);
-                var iconURL = $("<img src='http://openweathermap.org/img/wn/" + icon + "@2x.png' class = 'min-vw-15 mx-auto d-block' title='Day " + aux + " Weather: " + cond + "'></img>");
+                var iconURL = $("<img src='https://openweathermap.org/img/wn/" + icon + "@2x.png' class = 'min-vw-15 mx-auto d-block' title='Day " + aux + " Weather: " + cond + "'></img>");
                 var card = $("<p class ='card mh-100 h-auto d-inline-block align-items-center justify-content-center'></p>");
                 // card.attr("data-day", aux);
                 card.append(iconURL);
@@ -175,7 +179,7 @@ $(function (event) {
                 card.prepend(iconURL);
                 futCon.append(card);
                 aux++;
-                i = i + 7;
+                i = i + 7; //fast forward 24 hrs including the original increments on line 158
             }
         })
         ))
